ede7fb34c6dbd947e280e7b3cea735485f1c89c9
Jose Tapia <solidamorph@gmail.com>
2015-11-08 01:35:17 +0000

    Minor fixes

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -2,13 +2,17 @@ class ContactsController < ApplicationController
   def new
     @contact = Contact.new
   end
+  
   def create
     @contact = Contact.new(contact_params)
+    
     if @contact.save
       name = params[:contact][:name]
       email = params[:contact][:email]
       body = params[:contact][:comments]
+      
       ContactMailer.contact_email(name, email, body).deliver
+      
       flash[:success] = 'Message sent.'
       redirect_to new_contact_path
     else
@@ -16,4 +20,9 @@ class ContactsController < ApplicationController
         redirect_to new_contact_path
     end
   end
+  
+    private
+    def contact_params
+      params.require(:contact).permit(:name, :email, :comments)
+    end
 end
